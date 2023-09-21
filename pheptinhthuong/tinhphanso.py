154f866b797da755e42946399a467a8665f65dde
vuchitai <vuchitai6@gmail.com>
2023-09-21 20:21:16 +0700

    need fix error on the progaram

--- pheptinhthuong/tinhphanso.py
+++ pheptinhthuong/tinhphanso.py
@@ -4,7 +4,7 @@ def tinh_phan_so():
     number3 = Fraction(input("Nhập số đầu tiên để thực hiện phép tính: "))
     operations = input("Nhập dấu để tính ( cộng, trừ, nhân, hoặc chia ): ")
     number4 = Fraction(input("Nhập số thứ hai để thực hiện phép tính: "))
-
+    
     if operations == "cộng":
         result2 = number3 + number4
     elif operations == "trừ":
@@ -13,6 +13,5 @@ def tinh_phan_so():
         result2 = number3 * number4
     elif operations == "chia":
         result2 = number3 / number4
-    print("kết quả là:", result2)
+
     
-    return result2
