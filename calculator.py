50dbbac1c54290e0b00929ec962616575789d909
vuchitai <vuchitai6@gmail.com>
2023-10-27 22:45:44 +0700

    update v12

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import os
+from time import *
 def calculator():
     '''j cx dc'''
 import math
@@ -7,97 +8,111 @@ def clear_screen():
     
 pi = math.pi
 
+def countdown(seconds):
+    for i in range(seconds, 0, -1):
+        print(str(i) + "S")
+        sleep(1)
+        clear_screen()
+    print("Hết giờ!")
+
 
 while True:
-    clear_screen()
-    print("Bạn muốn tính toán cái gì?")
-    print("1. phép tính thường")
-    print("2. chu vi / diện tích của các loại hình phẳng")
-    print("3. chu vi / diện tích / thể tích của các loại hình không gian")
-    print("4. tìm ƯCLN / BCNN")
-    print("5. thoát")
-    choice = int(input("Nhập số để lựa chọn: "))
-    
-    if choice == 1:
+    try:
         clear_screen()
-        print("Bạn đã chọn tính phép tính thường")
-        print("Lưu ý: căn bậc 3 của 27 là '27^(1/3)'")
-        phep_tinh = input("nhập phép tính: ")
-        phep_tinh = phep_tinh.replace("^", "**")
-        print(eval(phep_tinh))
-
-
-    elif choice == 2:
-        clear_screen()
-        print("Bạn đã chọn tính chu vi / diện tích của các loại hình phẳng.")
-        print("Bạn muốn tính kiểu tính gì?")
-        print("1. chu vi")
-        print("2. diện tích")
-        choose = int(input("Nhập số để lựa chọn: "))
-
-        if choose == 1:
+        print("Bạn muốn tính toán cái gì?")
+        print("1. phép tính thường")
+        print("2. chu vi / diện tích của các loại hình phẳng")
+        print("3. chu vi / diện tích / thể tích của các loại hình không gian")
+        print("4. tìm ƯCLN / BCNN")
+        print("5. thoát")
+        choice = int(input("Nhập số để lựa chọn: "))
+        
+        if choice == 1:
             clear_screen()
-            from hinhphang.tinhchuvi import tinh_chu_vi
-            tinh_chu_vi()
-            
-        elif choose == 2:
-            clear_screen()
-            from hinhphang.tinhdientich import tinh_dien_tich
-            tinh_dien_tich()
-            
-    elif choice == 3:
-        clear_screen()
-        print("Bạn đã chọn tính chu vi / diện tích / thể tích của các hình không gian")
-        print("Bạn muốn tính kiểu gì?")
-        print("1. chu vi")
-        print("2. diện tích")
-        print("3. thể tích")
-        option = int(input("Nhập số để lựa chọn: "))
+            print("Bạn đã chọn tính phép tính thường")
+            print("Lưu ý: căn bậc 3 của 27 là '27^(1/3)'")
+            phep_tinh = input("nhập phép tính: ")
+            phep_tinh = phep_tinh.replace("^", "**")
+            print(eval(phep_tinh))
 
-        if option == 1:
-            clear_screen()
-            from hinhkogian.chuvi.tinhchuvikogian import tinh_chu_vi
-            tinh_chu_vi()
 
-        elif option == 2:
+        elif choice == 2:
             clear_screen()
-            print("Bạn đã chọn tính diện tích")
-            print("Bạn muốn tính kiểu tính diện tích nào?")
-            print("1. Sxq: diện tích xung quanh")
-            print("2. Stp: diện tích toàn phần ")
-            decision = int(input("Nhập số để lựa chọn: "))
+            print("Bạn đã chọn tính chu vi / diện tích của các loại hình phẳng.")
+            print("Bạn muốn tính kiểu tính gì?")
+            print("1. chu vi")
+            print("2. diện tích")
+            choose = int(input("Nhập số để lựa chọn: "))
 
-            if decision == 1:
+            if choose == 1:
                 clear_screen()
-                from hinhkogian.dientich.Sxq.Sxq import tinh_Sxq
-                tinh_Sxq()
-
-            elif decision == 2:
-                clear_screen()
-                from hinhkogian.dientich.Stp.Stp import tinh_Stp
-                tinh_Stp()
+                from hinhphang.tinhchuvi import tinh_chu_vi
+                tinh_chu_vi()
                 
-                        
-        elif option == 3:
+            elif choose == 2:
+                clear_screen()
+                from hinhphang.tinhdientich import tinh_dien_tich
+                tinh_dien_tich()
+                
+        elif choice == 3:
             clear_screen()
-            from hinhkogian.thetich.tinhthetich import tinh_the_tich
-            tinh_the_tich()
-            
+            print("Bạn đã chọn tính chu vi / diện tích / thể tích của các hình không gian")
+            print("Bạn muốn tính kiểu gì?")
+            print("1. chu vi")
+            print("2. diện tích")
+            print("3. thể tích")
+            option = int(input("Nhập số để lựa chọn: "))
 
-    elif choice == 4:
+            if option == 1:
+                clear_screen()
+                from hinhkogian.chuvi.tinhchuvikogian import tinh_chu_vi
+                tinh_chu_vi()
+
+            elif option == 2:
+                clear_screen()
+                print("Bạn đã chọn tính diện tích")
+                print("Bạn muốn tính kiểu tính diện tích nào?")
+                print("1. Sxq: diện tích xung quanh")
+                print("2. Stp: diện tích toàn phần ")
+                decision = int(input("Nhập số để lựa chọn: "))
+
+                if decision == 1:
+                    clear_screen()
+                    from hinhkogian.dientich.Sxq.Sxq import tinh_Sxq
+                    tinh_Sxq()
+
+                elif decision == 2:
+                    clear_screen()
+                    from hinhkogian.dientich.Stp.Stp import tinh_Stp
+                    tinh_Stp()
+                    
+                            
+            elif option == 3:
+                clear_screen()
+                from hinhkogian.thetich.tinhthetich import tinh_the_tich
+                tinh_the_tich()
+                
+
+        elif choice == 4:
+            clear_screen()
+            print("Bạn đã chọn tìm ƯCLN BCNN")
+            from timso import tim_uoc_chung_va_boi_chung_cua_nhieu_so
+            tim_uoc_chung_va_boi_chung_cua_nhieu_so()
+
+        
+        elif choice == 5:
+            clear_screen()
+            print("cảm ơn bạn đã sử dụng.")
+            break
+
+        cont = input("Tiếp tục tính toán? (có/không): ").lower()
+        if not cont.islower() or cont != 'có':
+            clear_screen() 
+            print("cảm ơn bạn đã sử dụng.")
+            break
+    except:
+        print("Value error!")
+        print("Chương trình sẽ khởi động lại sau 5s")
+        sleep(1)
         clear_screen()
-        print("Bạn đã chọn tìm ƯCLN BCNN")
-        from timso import tim_uoc_chung_va_boi_chung_cua_nhieu_so
-        tim_uoc_chung_va_boi_chung_cua_nhieu_so()
-
-    
-    elif choice == 5:
-        clear_screen()
-        print("cảm ơn bạn đã sử dụng.")
-        break
-
-    cont = input("Tiếp tục tính toán? (có/không): ").lower()
-    if not cont.islower() or cont != 'có':
-        clear_screen() 
-        print("cảm ơn bạn đã sử dụng.")
-        break
+        countdown(5);
--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-phep_tinh = input("nhập phép tính: ")
-phep_tinh = phep_tinh.replace("^", "**")
-print(eval(phep_tinh))
+while True: 
+    try:
+        print("Bạn muốn tính toán cái gì?")
+        print("1. phép tính thường")
+        print("2. chu vi / diện tích của các loại hình phẳng")
+        print("3. chu vi / diện tích / thể tích của các loại hình không gian")
+        print("4. tìm ƯCLN / BCNN")
+        print("5. thoát")
+        choice = int(input("Nhập số để lựa chọn: "))
+        
+        if choice == 1:
+            print("1")
+            
+    except ValueError:
+        print("ko hợp lệ")
+        
